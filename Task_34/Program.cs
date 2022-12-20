42be251bc1208809e29f19d1123f8c0baff98408
Timerkhan <timerkhan12@gmail.com>
2022-12-20 12:23:56 +0500

    done

---
+++ Task_34/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int SIZE = Convert.ToInt32(Console.ReadLine());
+int LEFT_RANGE = 100;
+int RIGHT_RANGE = 1000;
+
+int[] NewArr (int SIZE, int LEFT_RANGE, int RIGHT_RANGE)
+{
+    int[] arr = new int[SIZE];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(LEFT_RANGE, RIGHT_RANGE);
+    
+    }
+    return arr;
+}
+
+void ShowArr (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine(arr[i]);
+    }
+}
+
+int GetNumber (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] % 2 == 0){
+            count = count +1;
+        }
+    return count; 
+}
+
+int[] Arr = NewArr(SIZE, LEFT_RANGE, RIGHT_RANGE);
+ShowArr(Arr);
+int Number =  GetNumber(Arr);
+System.Console.WriteLine("Ответ: ");
+System.Console.Write(Number);
